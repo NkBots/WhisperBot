3d39080314bf64b7d50e2ce41b8fef5ed762780d
NkBots <116135526+NkBots@users.noreply.github.com>
2023-04-13 19:41:45 +0530

    Update start.py

--- WhisperBot/start.py
+++ WhisperBot/start.py
@@ -7,10 +7,10 @@ from pyrogram.types import InlineKeyboardMarkup
 @Client.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot, msg):
 	user = await bot.get_me()
-	mention = user["mention"]
+	
 	await bot.send_sticker(msg.chat.id, "CAACAgIAAxkBAAIal2EVKvGYCpidwcjowvL-j8zAB9RcAAK-DAACX_g4ShXqde_-mMrnHgQ")
 	await bot.send_message(
 		msg.chat.id,
-		Data.START.format(msg.from_user.mention, mention),
+		Data.START.format{msg.from_user.mention},
 		reply_markup=InlineKeyboardMarkup(Data.buttons),
 	)
